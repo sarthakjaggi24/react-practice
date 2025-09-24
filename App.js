c1f82727e72f29bc297771033c4b04656925705a
sarthakjaggi24 <jaggisarthak4@gmail.com>
2025-09-24 20:54:57 +0530

    files updated

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello This is the content of h1 tag"),
-    React.createElement("h2", {}, "This is h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hello This is the content of h1 tag"),
-    React.createElement("h2", {}, "This is h2 tag"),
-  ])
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-console.log(parent); // object
+// React.createElement ==> (react element = js object) ==> HTML element using "render"
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello");
+
+console.log(heading);
+
+// JSX is not html inside js, jsx is just jsx basically it is a syntax which browsers cannot understand, and JSX gets transpiled before it reaches the js engine or browser because our browser or js engine cannot understand jsx, and this transpiled is done using a js compiler/transpiler which is being installed with parcel and it is known as "babel" which is present in "node_modules".
+
+// "transpile" means converting a language/syntax into another, and here jsx is being converted into js.
+
+// and how this jsx is transpiled? first jsx is converted to react element which is = to js object and then it is converted to html element using render.
+
+const jsxheading = <h1 id="jsxheading">This is a jsx heading</h1>;
+
+// upper line is converted to this ---> React.createElement("h1", {id:"jsxheading"}, "This is a jsx heading")
+
+console.log(jsxheading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxheading);
